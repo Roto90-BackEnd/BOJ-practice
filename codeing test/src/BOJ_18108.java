5953c89441e99a4719c27ee48e873ad110447b34
Roto90 <slalala90@gmail.com>
2025-10-22 15:50:37 +0900

    feat: [박태준] BOJ_18108번 완료
    BOJ_18108번 완료

---
+++ codeing test/src/BOJ_18108.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int buddhistYear = scanner.nextInt();
+
+        System.out.println(buddhistYear - 543);
+
+        scanner.close();
+    }
+}
